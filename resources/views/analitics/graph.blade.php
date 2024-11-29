f3116d865513272acfd50af23d3e8715d0cb0f40
lakshmikr31 <lakshmikr2691@gmail.com>
2024-11-29 18:13:31 +0530

    testing git setup

--- resources/views/analitics/graph.blade.php
+++ resources/views/analitics/graph.blade.php
@@ -1,19 +1,4 @@
 
-<link rel="stylesheet" href="{{ asset($constants['CSSFILEPATH'] . 'common.css') }}">
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<style>
-    .custom-safety-btn.active {
-        border-color: yellow;
-        background-color: #f7f7f7;
-        color: #333;
-    }
-
-    .custom-safety-btn {
-        border-color: transparent;
-        background-color: #ffffff;
-        color: #333;
-    }
-</style>
 <div class="graph-area rounded mt-6 relative">
     <div class="graph-hidden-area">
         <div class="pr-3 pt-4 flex gap-3 justify-end">
@@ -42,11 +27,3 @@
     </div>
 </div>
 
-@section('scripts')
-<script src="{{ asset($constants['JSFILEPATH'] . 'analitics.js') }}"></script>
-<script src="{{ asset($constants['JSFILEPATH'] . 'graph-setup.js') }}"></script>
-<script src="{{ asset($constants['JSFILEPATH'] . 'reports-analytics.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/chart.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/helpers.esm.min.js">
-</script>
-@endsection
